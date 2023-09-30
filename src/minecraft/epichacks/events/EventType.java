4bbc15b912070156ed955939b8dfebdd0dc182c2
William Huynh <william.huynh5@gmail.com>
2023-09-30 13:58:21 +1000

    Refactored Fly Hack Module

--- src/minecraft/epichacks/events/EventType.java
+++ src/minecraft/epichacks/events/EventType.java
@@ -1,6 +1,20 @@
 package epichacks.events;
 
+/**
+ * The {@code EventType} enum represents the type of an event, which can be either "PRE" or "POST".
+ * Event types are used to categorize when an event occurs in a sequence of actions or operations.
+ */
 public enum EventType {
-	PRE,
-	POST
+    /**
+     * Represents an event that occurs before the main action or operation.
+     * Typically, "PRE" events are used for preparation or setup actions.
+     */
+    PRE,
+
+    /**
+     * Represents an event that occurs after the main action or operation.
+     * Typically, "POST" events are used for cleanup or follow-up actions.
+     */
+    POST
 }
+
--- src/minecraft/epichacks/modules/movenment/Fly.java
+++ src/minecraft/epichacks/modules/movenment/Fly.java
@@ -5,28 +5,52 @@ import epichacks.events.Event;
 import epichacks.events.listeners.EventUpdate;
 import epichacks.modules.Module;
 
+/**
+ * The {@code Fly} class represents a hack that enables flight in the game.
+ * It extends the {@link epichacks.modules.Module} class and provides specific
+ * behavior for enabling and disabling flight.
+ */
 public class Fly extends Module {
 
-	public Fly() {
-		super("fly", Keyboard.KEY_G, Category.MOVEMENT);
-	}
-	
-	public void onEnable() {
-		mc.thePlayer.capabilities.isFlying = true;
-		mc.thePlayer.capabilities.allowFlying = true;
-	}
-	
-	public void onDisable() {
-		mc.thePlayer.capabilities.isFlying = false;
-		mc.thePlayer.capabilities.allowFlying = false;
-	}
-	
-	public void onEvent(Event e) {
-		if (e instanceof EventUpdate) {
-			if (e.isPre()) {
-				
-			}
-		}
-	}
-	
+    /**
+     * Constructs a new {@code Fly} hack with the default name, key code, and category.
+     * The default key code is {@link org.lwjgl.input.Keyboard#KEY_G}.
+     */
+    public Fly() {
+        super("fly", Keyboard.KEY_G, Category.MOVEMENT);
+    }
+
+    /**
+     * Called when the hack is enabled.
+     * This method sets the player's flight capabilities to allow flying.
+     */
+    @Override
+    public void onEnable() {
+        mc.thePlayer.capabilities.isFlying = true;
+        mc.thePlayer.capabilities.allowFlying = true;
+    }
+
+    /**
+     * Called when the hack is disabled.
+     * This method disables the player's flight capabilities.
+     */
+    @Override
+    public void onDisable() {
+        mc.thePlayer.capabilities.isFlying = false;
+        mc.thePlayer.capabilities.allowFlying = false;
+    }
+
+    /**
+     * Handles an event, specifically the {@link epichacks.events.listeners.EventUpdate} event.
+     *
+     * @param e The event to be handled.
+     */
+    @Override
+    public void onEvent(Event e) {
+        if (e instanceof EventUpdate) {
+            if (e.isPre()) {
+                // Additional logic specific to the EventUpdate can be added here.
+            }
+        }
+    }
 }
