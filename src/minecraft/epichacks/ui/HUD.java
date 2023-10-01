2c47b3ff4c371324c592d435f0b6ce0ce257a310
William Huynh <william.huynh5@gmail.com>
2023-10-01 14:17:47 +1100

    added javadoc to HUD.java

--- src/minecraft/epichacks/ui/HUD.java
+++ src/minecraft/epichacks/ui/HUD.java
@@ -8,27 +8,50 @@ import epichacks.modules.Module;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.ScaledResolution;
 
+/**
+ * The {@code HUD} class represents a heads-up display (HUD) for displaying information and status of various modules.
+ * It is responsible for rendering module names on the screen when those modules are enabled.
+ */
 public class HUD {
-	
-	public Minecraft minecraft = Minecraft.getMinecraft();
-	final Integer defaultXCoord = 4;
-	final Integer defaultYCoord = 4;
-	final Integer defaultZCoord = -1;
-	final Integer yDisplacement = 10;
-	
-	public void draw() {
-		ScaledResolution scaledResolutiojn = new ScaledResolution(minecraft, minecraft.displayWidth, minecraft.displayHeight);
-		// minecraft.fontRendererObj.drawString(Client.name + " v" + Client.version, defaultXCoord, defaultYCoord, defaultZCoord);
-		Integer yCoord = defaultYCoord;
-		for (Entry<Integer, Module> entry: Client.modules.entrySet()) {
-			Module hack = entry.getValue();
-			if (hack.isEnabled()) {
-				minecraft.fontRendererObj.drawString(entry.getValue().hackName, defaultXCoord, yCoord, defaultZCoord);
-				yCoord += yDisplacement;
-			}
-			
-		}
-		
-	}
-	
+    /**
+     * The instance of the Minecraft client for game-related operations.
+     */
+    public Minecraft minecraft = Minecraft.getMinecraft();
+
+    /**
+     * The default X-coordinate for rendering module names.
+     */
+    final Integer defaultXCoord = 4;
+
+    /**
+     * The default Y-coordinate for rendering the first module name.
+     */
+    final Integer defaultYCoord = 4;
+
+    /**
+     * The default Z-coordinate for rendering module names.
+     */
+    final Integer defaultZCoord = -1;
+
+    /**
+     * The vertical displacement between each module name when rendering.
+     */
+    final Integer yDisplacement = 10;
+
+    /**
+     * Draws the heads-up display by rendering the names of enabled modules on the screen.
+     */
+    public void draw() {
+        ScaledResolution scaledResolution = new ScaledResolution(minecraft, minecraft.displayWidth, minecraft.displayHeight);
+        Integer yCoord = defaultYCoord;
+
+        for (Entry<Integer, Module> entry : Client.modules.entrySet()) {
+            Module hack = entry.getValue();
+            if (hack.isEnabled()) {
+                // Render the name of the enabled module at the specified coordinates.
+                minecraft.fontRendererObj.drawString(entry.getValue().hackName, defaultXCoord, yCoord, defaultZCoord);
+                yCoord += yDisplacement; // Move to the next Y-coordinate for the next module.
+            }
+        }
+    }
 }
